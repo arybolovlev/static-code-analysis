6db49c08561e81941e0c296e41d4add575bbb417
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-10-20 15:27:23 +0200

    Update cmd package

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/arybolovlev/static-code-analysis/internal/analysis"
 )
@@ -9,8 +12,17 @@ import (
 func main() {
 	var path string
 	flag.StringVar(&path, "path", ".", "-path=/path")
-
+	var jsonOutput bool
+	flag.BoolVar(&jsonOutput, "json", true, "-json")
 	flag.Parse()
 
-	analysis.Analysis(path)
+	o := analysis.Analysis(path)
+
+	if jsonOutput {
+		jo, err := json.Marshal(o)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fmt.Println(string(jo))
+	}
 }
